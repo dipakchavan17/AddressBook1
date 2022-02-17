d15709d19c8dfa1bbe527538881693a16ca72f38
Dipak@1432 <chavandipakr17@gmail.com>
2022-02-17 22:55:26 +0530

    [dipak] No Duplicate Person

--- AdressBookImplementation/AdressBookImplementation/Program.cs
+++ AdressBookImplementation/AdressBookImplementation/Program.cs
@@ -11,6 +11,8 @@ namespace AdressBookImplementation
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to address book");
+            CreateAddressBook createAddressBook = new CreateAddressBook();
+            createAddressBook.ReadInput();
             Console.ReadLine();
         }
     }
